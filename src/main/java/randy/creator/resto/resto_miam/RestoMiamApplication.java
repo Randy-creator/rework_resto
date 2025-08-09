abc1454173923c740c53007614d99069e5246b4f
Randy-creator <hei.randy.2@gmail.com>
2025-08-09 21:52:55 +0300

    chore: init new spring boot project

---
+++ src/main/java/randy/creator/resto/resto_miam/RestoMiamApplication.java
@@ -0,0 +1,13 @@
+package randy.creator.resto.resto_miam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoMiamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestoMiamApplication.class, args);
+  }
+
+}
---
+++ src/test/java/randy/creator/resto/resto_miam/RestoMiamApplicationTests.java
@@ -0,0 +1,13 @@
+package randy.creator.resto.resto_miam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoMiamApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
